9c0ac858c4e00f90102e3f0235012e8b2c10f0db
John Rathgeber <john_rathgeber@brown.edu>
2026-02-24 17:42:03 -0500

    done iwth local store

--- distribution/local/store.js
+++ distribution/local/store.js
@@ -15,6 +15,10 @@
   Use the `path` module for that.
 */
 
+const { id } = require("../util/util.js");
+const fs = require('node:fs');
+const path = require('node:path');
+const util = require("../util/util.js");
 
 /**
  * @param {any} state
@@ -22,7 +26,44 @@
  * @param {Callback} callback
  */
 function put(state, configuration, callback) {
-  return callback(new Error('store.put not implemented'));
+  const serialized = util.serialize(state);
+  if (typeof configuration == "string") {
+    const alphanumeric = configuration.replace(/[^a-zA-Z0-9]/g, '');
+    const filePath = path.resolve("store", alphanumeric);
+    fs.writeFile(filePath, serialized, (e) => {
+      if (e) {
+        callback(new Error(`Error while putting a file into store: ${e.message}`));
+      }
+      else {
+        callback(null, state);
+      }
+    });
+  }
+  else if (configuration === null) {
+    const alphanumeric = id.getID(state).replace(/[^a-zA-Z0-9]/g, '');
+    const filePath = path.resolve("store", alphanumeric);
+    fs.writeFile(filePath, serialized, (e) => {
+      if (e) {
+        callback(new Error(`Error while putting a file into store: ${e.message}`));
+      }
+      else {
+        callback(null, state);
+      }
+    });
+  }
+  else {
+    const alphanumeric = (configuration.key).replace(/[^a-zA-Z0-9]/g, '');
+    const filePath = path.resolve("store", alphanumeric);
+    fs.writeFile(filePath, serialized, (e) => {
+      if (e) {
+        callback(new Error(`Error while putting a file into store: ${e.message}`));
+      }
+      else {
+        callback(null, state);
+      }
+    });
+    // don't know what to do with gid yet.
+  }
 }
 
 /**
@@ -30,7 +71,28 @@ function put(state, configuration, callback) {
  * @param {Callback} callback
  */
 function get(configuration, callback) {
-  return callback(new Error('store.get not implemented'));
+  if (typeof configuration == "string") {
+    try {
+      const alphanumeric = configuration.replace(/[^a-zA-Z0-9]/g, '');
+      const filePath = path.resolve("store", alphanumeric);
+      const data = fs.readFileSync(filePath, 'utf8');
+      callback(null, util.deserialize(data));
+    }
+    catch (e) {
+      callback(new Error(`Error while getting a file from store: ${e.message}`));
+    }
+  }
+  else {
+    try {
+      const alphanumeric = (configuration.key).replace(/[^a-zA-Z0-9]/g, '');
+      const filePath = path.resolve("store", alphanumeric);
+      const data = fs.readFileSync(filePath, 'utf8');
+      callback(null, util.deserialize(data));
+    }
+    catch (e) {
+      callback(new Error(`Error while getting a file from store: ${e.message}`));
+    }
+  }
 }
 
 /**
@@ -38,7 +100,30 @@ function get(configuration, callback) {
  * @param {Callback} callback
  */
 function del(configuration, callback) {
-  return callback(new Error('store.del not implemented'));
+  if (typeof configuration == "string") {
+    try {
+      const alphanumeric = configuration.replace(/[^a-zA-Z0-9]/g, '');
+      const filePath = path.resolve("store", alphanumeric);
+      const data = fs.readFileSync(filePath, 'utf8');
+      fs.unlinkSync(filePath);
+      callback(null, util.deserialize(data));
+    }
+    catch (e) {
+      callback(new Error(`Error while deleting a file from store: ${e.message}`));
+    }
+  }
+  else {
+    try {
+      const alphanumeric = (configuration.key).replace(/[^a-zA-Z0-9]/g, '');
+      const filePath = path.resolve("store", alphanumeric);
+      const data = fs.readFileSync(filePath, 'utf8');
+      fs.unlinkSync(filePath);
+      callback(null, util.deserialize(data));
+    }
+    catch (e) {
+      callback(new Error(`Error while deleting a file from store: ${e.message}`));
+    }
+  }
 }
 
 /**
