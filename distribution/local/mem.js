82c0889ae539680da1d0e63016e8ed76351a26b1
John Rathgeber <john_rathgeber@brown.edu>
2026-02-23 23:20:37 -0500

    finished local mem

--- distribution/local/mem.js
+++ distribution/local/mem.js
@@ -1,3 +1,29 @@
+// @ts-check
+/**
+ * @typedef {import("../types.js").Callback} Callback
+ * @typedef {import("../types.js").Config} Config
+ * @typedef {import("../types.js").Node} Node
+ */
+
+const { id } = require("../util/util.js");
+
+
+/**
+ * @typedef {Object} StoreConfig
+ * @property {string | null} key
+ * @property {string} gid
+ *
+ * @typedef {StoreConfig | string | null} SimpleConfig
+ *
+ * @typedef {Object} Mem
+ * @property {(configuration: SimpleConfig, callback: Callback) => void} get
+ * @property {(state: any, configuration: SimpleConfig, callback: Callback) => void} put
+ * @property {(state: any, configuration: SimpleConfig, callback: Callback) => void} append
+ * @property {(configuration: SimpleConfig, callback: Callback) => void} del
+ * @property {(configuration: Object.<string, Node>, callback: Callback) => void} reconf
+ */
+
+let mp = {};
 
 /**
  * @param {any} state
@@ -5,7 +31,17 @@
  * @param {Callback} callback
  */
 function put(state, configuration, callback) {
-  return callback(new Error('mem.put not implemented'));
+  if (typeof configuration == "string") {
+    mp[configuration] = state;
+  }
+  else if (configuration === null) {
+    mp[id.getID(state)] = state;
+  }
+  else {
+    mp[configuration.key] = state;
+    // don't know what to do with gid yet.
+  }
+  callback(null, state);
 };
 
 /**
@@ -22,7 +58,22 @@ function append(state, configuration, callback) {
  * @param {Callback} callback
  */
 function get(configuration, callback) {
-  return callback(new Error('mem.get not implemented'));
+  if (typeof configuration == "string") {
+    if (configuration in mp) {
+      callback(null, mp[configuration]);
+    }
+    else {
+      callback(new Error(`Given configuration not found in map: ${configuration}`));
+    }
+  }
+  else {
+    if (configuration.key in mp) {
+      callback(null, mp[configuration.key]);
+    }
+    else {
+      callback(new Error(`Given configuration not found in map: ${configuration}`));
+    }
+  }
 }
 
 /**
@@ -30,7 +81,26 @@ function get(configuration, callback) {
  * @param {Callback} callback
  */
 function del(configuration, callback) {
-  return callback(new Error('mem.del not implemented'));
+  if (typeof configuration == "string") {
+    if (configuration in mp) {
+      const r = mp[configuration];
+      delete mp[configuration];
+      callback(null, r);
+    }
+    else {
+      callback(new Error(`Given configuration not found in map: ${configuration}`));
+    }
+  }
+  else {
+    if (configuration.key in mp) {
+      const r = mp[configuration.key];
+      delete mp[configuration.key];
+      callback(null, r);
+    }
+    else {
+      callback(new Error(`Given configuration not found in map: ${configuration}`));
+    }
+  }
 };
 
 module.exports = {put, get, del, append};
